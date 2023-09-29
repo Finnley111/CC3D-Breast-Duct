d9f104e6b29edbbec093cd2c5ffd442e0a4e57c6
Finnley111 <86789658+Finnley111@users.noreply.github.com>
2023-09-29 16:33:00 -0400

    Edited Position Tracking Graph
    -Graph is now in its own steppable for orginization
    -The graph erases all data points every 100 mcs to track the most recent positions only

--- Simulation/BreastDuctSimSteppables.py
+++ Simulation/BreastDuctSimSteppables.py
@@ -33,22 +33,7 @@ class BreastDuctSim(SteppableBasePy):
     def __init__(self,frequency=1):
 
         SteppableBasePy.__init__(self,frequency)
-
-
-    def start(self):
-            
-        # make a plot of the cells positions
-        self.plot_win = self.add_new_plot_window(title='MEM COM Track',
-                                                 x_axis_title='X', x_scale_type='linear',
-                                                 y_axis_title='Y', y_scale_type='linear',
-                                                 grid=False)
-        self.plot_win.add_plot("Track", style='dot', color='white', size=1)
-        # make some dots to force the plot to autoscale like we want (0,0),(100,100)
-        # arguments are (name of the data series, x, y)
-        self.plot_win.add_data_point("Track",0,    0)
-        self.plot_win.add_data_point("Track",0,  100)
-        self.plot_win.add_data_point("Track",100,  0)
-        self.plot_win.add_data_point("Track",100,100)
+ 
 
     # CELL KILLER CODE/ LIMITS NUMBER OF EACH CELL TYPE
     def step(self,mcs):
@@ -79,61 +64,7 @@ class BreastDuctSim(SteppableBasePy):
             # #print('Neighbor count for cell.id={} is {}'.format(cell.id, neighbor_count_by_type_dict))
             # if 1 not in neighbor_count_by_type_dict:
                 # print(neighbor_count_by_type_dict)
-              
-              
-        # This is not really needed, can delete later same with code above that create the graph
-        # just tracking the center of "MEM" type every 100th MCS
-        if mcs % 10 == 0:
-            for cell in self.cell_list_by_type(self.MEM):
-                # using the plot:
-                # THIS TRACKS THE CENTER OF MASS OF MEM
-                #self.plot_win.add_data_point("Track",cell.xCOM,cell.yCOM)
-                
-                # THIS TRACKS EACH PIXEL OF MEM
-                pixel_list = self.get_cell_pixel_list(cell)
-                for pixel_tracker_data in pixel_list:
-                    x = pixel_tracker_data.pixel.x
-                    y = pixel_tracker_data.pixel.y
-                    self.plot_win.add_data_point("Track",x,y)
-                        
-               
-        # Make sure ExternalPotential plugin is loaded
-        # negative lambdaVecX makes force point in the positive direction
-        # THIS CONTROLS THE MOVEMENT OF THE MACROPHAGE
 
-        mac_X = 0.0
-        mac_Y = 0.0
-        lamX_lower_bound = -0.5
-        lamX_upper_bound = 10.5 # 0.5 originally
-        lamY_lower_bound = -0.5
-        lamY_lower_bound = 10.5 # 0.5 originally
-        num_of_mem_cells = 0
-        pos_of_mems = []
-
-        # get position of macrophage
-        for cell in self.cell_list_by_type(self.MAC):
-            mac_X = cell.xCOM
-            mac_Y = cell.yCOM
-            print(mac_X, ' ', mac_Y)
-            
-        # LOOK UP: HOW TO GET THE VECTOR FROM TWO POINTS TO CONTROL THE DIRECTION OF THE MACROPHAGE
-            
-        
-        # get position and number of membrane cells
-        for cell in self.cell_list_by_type(self.MEM):
-
-            pos_of_mems.append((cell.xCOM, cell.yCOM))
-        
-        num_of_mem_cells = len(pos_of_mems)
-            
-        
-        
-        for cell in self.cell_list_by_type(self.MAC):
-            # force component pointing along X axis
-            cell.lambdaVecX = 10.1 * random.uniform(lamX_lower_bound, lamX_upper_bound)
-
-            # force component pointing along Y axis
-            cell.lambdaVecY = 10.1 * random.uniform(lamY_lower_bound, lamX_upper_bound)
 
     def finish(self):
         """
@@ -143,6 +74,7 @@ class BreastDuctSim(SteppableBasePy):
     def on_stop(self):
         # this gets called each time user stops simulation
         return
+        
 
 
 class GrowthSteppable(SteppableBasePy):
@@ -165,8 +97,7 @@ class GrowthSteppable(SteppableBasePy):
                 # # you can access/manipulate cell properties here
                 # x = pixel_tracker_data.pixel.x
                 # y = pixel_tracker_data.pixel.y
-                
-                
+
 
 
 class MitosisSteppable(MitosisSteppableBase):
@@ -221,4 +152,118 @@ class MitosisSteppable(MitosisSteppableBase):
             # self.child_cell.type=2
         # else:
             # self.child_cell.type=1
+
+
+class CellMovementSteppable(SteppableBasePy):
+    def __init__(self, frequency=1):
+        '''
+        constructor
+        '''
+        SteppableBasePy.__init__(self, frequency)
         
+
+    def start(self):
+        
+        print("CellMovementSteppable: This function is called once before simulation")
+
+    def step(self, mcs):
+        '''
+        called every MCS or every "frequency" MCS (depending how it was instantiated in the main Python file)
+        '''
+        # Make sure ExternalPotential plugin is loaded
+        # negative lambdaVecX makes force point in the positive direction
+        # THIS CONTROLS THE MOVEMENT OF THE MACROPHAGE
+
+        mac_X = 0.0
+        mac_Y = 0.0
+        lamX_lower_bound = -0.5
+        lamX_upper_bound = 0.5 # 0.5 originally
+        lamY_lower_bound = -0.5
+        lamY_lower_bound = 0.5 # 0.5 originally
+        num_of_mem_cells = 0
+        pos_of_mems = []
+
+        # get position of macrophage
+        for cell in self.cell_list_by_type(self.MAC):
+            mac_X = cell.xCOM
+            mac_Y = cell.yCOM
+            print(mac_X, ' ', mac_Y)
+            
+        # LOOK UP: HOW TO GET THE VECTOR FROM TWO POINTS TO CONTROL THE DIRECTION OF THE MACROPHAGE
+            
+        
+        # get position and number of membrane cells
+        for cell in self.cell_list_by_type(self.MEM):
+
+            pos_of_mems.append((cell.xCOM, cell.yCOM))
+        
+        num_of_mem_cells = len(pos_of_mems)
+            
+        
+        
+        for cell in self.cell_list_by_type(self.MAC):
+            # force component pointing along X axis
+            cell.lambdaVecX = 10.1 * random.uniform(lamX_lower_bound, lamX_upper_bound)
+
+            # force component pointing along Y axis
+            cell.lambdaVecY = 10.1 * random.uniform(lamY_lower_bound, lamX_upper_bound)
+
+    def finish(self):
+        '''
+        this function may be called at the end of simulation - used very infrequently though
+        '''        
+        
+        return
+
+    def on_stop(self):
+        '''
+        this gets called each time user stops simulation
+        '''        
+        
+        return
+
+
+class PostionPlotSteppable(SteppableBasePy):
+    
+    def __init__(self, frequency=10):
+        SteppableBasePy.__init__(self, frequency)
+
+
+    def start(self):
+        # make a plot of the cells positions
+        self.plot_win = self.add_new_plot_window(title='MEM COM Track',
+                                                 x_axis_title='X', x_scale_type='linear',
+                                                 y_axis_title='Y', y_scale_type='linear',
+                                                 grid=False)
+        self.plot_win.add_plot("Track", style='dot', color='white', size=1)
+        # make some dots to force the plot to autoscale like we want (0,0),(100,100)
+        # arguments are (name of the data series, x, y)
+        self.plot_win.add_data_point("Track",0,    0)
+        self.plot_win.add_data_point("Track",0,  100)
+        self.plot_win.add_data_point("Track",100,  0)
+        self.plot_win.add_data_point("Track",100,100)
+
+
+    def step(self, mcs):
+         
+        # This is not really needed, can delete later same with code above that create the graph
+        # just tracking the center of "MEM" type every 100th MCS
+        if mcs % 10 == 0:
+            for cell in self.cell_list_by_type(self.MEM):
+                # using the plot:
+                # THIS TRACKS THE CENTER OF MASS OF MEM
+                #self.plot_win.add_data_point("Track",cell.xCOM,cell.yCOM)
+                
+                # THIS TRACKS EACH PIXEL OF MEM
+                pixel_list = self.get_cell_pixel_list(cell)
+                for pixel_tracker_data in pixel_list:
+                    x = pixel_tracker_data.pixel.x
+                    y = pixel_tracker_data.pixel.y
+                    self.plot_win.add_data_point("Track",x,y)
+                    
+        if mcs % 100 == 0:
+            self.plot_win.erase_all_data()
+            self.plot_win.add_data_point("Track",0,    0)
+            self.plot_win.add_data_point("Track",0,  100)
+            self.plot_win.add_data_point("Track",100,  0)
+            self.plot_win.add_data_point("Track",100,100)
